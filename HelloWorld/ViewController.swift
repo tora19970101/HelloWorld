f2af2b170fbba30ec770fc99ab3581dc80616f52
平澤裕美 <hirasawahiromi@hirasawahiromi-no-macbook-pro.local>
2016-12-02 18:38:56 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 平澤裕美 on 2016/12/02.
+//  Copyright © 2016年 toranosuke.sekine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
